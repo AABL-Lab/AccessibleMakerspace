c350393add86991c534f7bdf768d0d4136b66bac
Tylerch18 <140772365+Tylerch18@users.noreply.github.com>
2026-01-10 01:04:44 -0500

    Fix contact mailto link

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,42 +1,46 @@
 import React from "react";
-// TODO: import icons 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
-const footer = () => {
+const Footer = () => {
+
+  const handleMailto = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const subject = form.subject.value;
+    const message = form.message.value;
+
+    const bodyText = `Hi there!\n${message}`;
+
+    const mailtoLink = `mailto:a2makerspace@eecs.tufts.edu?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(bodyText)}`;
+    window.location.href = mailtoLink;
+  };
+
   return (
-    <div class="footer">
-      <div class="row">
-        <div class="location">
-          <h4 class="contact">Contact Us</h4>
-          <a href="https://shorturl.at/jlz12" target="blank"> 177 College Ave, Medford, MA 02155</a>
-          <p class="mb-0"><i class="fa fa-phone mr-3"></i>(617) 627-6724</p>
-          <p> <i class="fa fa-envelope-o mr-3"></i> a2makerspace@eecs.tufts.edu</p>
+    <div className="footer">
+      <div className="row">
+        <div className="location">
+          <h4 className="contact">Contact Us</h4>
+          <a href="https://shorturl.at/jlz12" target="_blank" rel="noreferrer"> 177 College Ave, Medford, MA 02155</a>
+          <p className="mb-0"><i className="fa fa-phone mr-3"></i>(617) 627-6724</p>
+          <p> <i className="fa fa-envelope-o mr-3"></i> a2makerspace@eecs.tufts.edu</p>
         </div>
       </div> 
 
       <div>
         <div id="right-col">
-          {/* Consider changing email method to not open browser */}
-          {/* This link looks promising */}
-          {/* https://www.geeksforgeeks.org/how-to-send-an-email-from-javascript/# */}
-          {/* https://mailtrap.io/blog/javascript-send-email/ */}
-          {/* React Mail */}
-          <form className="form" action="mailto:a2makerspace@eecs.tufts.edu" method="get" enctype="text/plain">
+          <form className="form" onSubmit={handleMailto}>
             <h2 style={{textAlign: "center"}}>Get in Touch</h2> 
+            
             <label>
               Subject:
-              <input type="text" subject="subject" placeholder="Subject" required/>
-            </label>
-
-            <label>
-              Email:
-              <input type="text" email="email" placeholder="Email" required/>
+              <input type="text" name="subject" placeholder="Subject" required/>
             </label>
             
             <label>
               Message:
-              <input type="text" message="message" placeholder="Message" required/>
+              <input type="text" name="message" placeholder="Message" required/>
             </label>
 
             <input type="submit" value="Send Message" />
@@ -47,4 +51,4 @@ const footer = () => {
   );
 }
 
-export default footer;
+export default Footer;
